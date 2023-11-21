0e99eac43ffd4935d67e4fff9898bf0af030c71c
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-21 12:10:08 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const btnComenzarJuego = document.querySelector('#btnComenzarJuego');
+const formNumero = document.getElementById('formNumero');
+let numeroGenerado;
+
+const numeroAleatorio = () =>{
+    const numGenerado = Math.floor(Math.random()*10) + 1;
+    console.log(numGenerado)
+    return numGenerado;
+}
+
+const iniciarJuego = () =>{
+    numeroGenerado = numeroAleatorio();
+    formNumero.value = '';
+}
+
+const adivinoNumero = (numeroIngresado, numeroGenerado) => numeroIngresado === numeroGenerado;
+
+const obtenerNumeroIngresado = () => {
+    const input = document.getElementById('numero');
+    const numIngresado = parseInt(input.value);
+    return numIngresado;
+}
+
+const verificarNumeroIngresado = (num) => !isNaN(num) && num >= 1 && num <= 10;
+
+const verificarEnvio = (e) => {
+    const numeroIngresado = obtenerNumeroIngresado();
+    e.preventDefault();
+
+    if (verificarNumeroIngresado(numeroIngresado)) {
+        if(adivinoNumero(numeroIngresado,numeroGenerado)){
+            alert(`Adivinaste, el número era ${numeroGenerado}`);
+        }else{
+            alert(`Fallaste, el número era ${numeroGenerado}, intentalo de nuevo`);
+        }
+        console.log(formNumero.value);
+    } else {
+        alert('Ingresa un número válido');
+    }
+};
+
+
+btnComenzarJuego.addEventListener('click', iniciarJuego);
+formNumero.addEventListener('submit', verificarEnvio);
